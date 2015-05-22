fb710820b9a1b64f5bbab563dd6c63d0e2b5092c
Kazuhiko Maeda <shirokumasensei@gmail.com>
2015-05-22 12:46:59 +0900

    quote追加…

--- gulpfile.js
+++ gulpfile.js
@@ -94,9 +94,21 @@ gulp.task('build-post-link-html', function() {
     .pipe(gulp.dest('./dist/'));
 });
 
+gulp.task('build-post-quote-short-html', function() {
+  return gulp.src(['./html/header.html', './html/post-quote-short.html', './html/footer.html'])
+    .pipe(concat('post-quote-short.html'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('build-post-quote-long-html', function() {
+  return gulp.src(['./html/header.html', './html/post-quote-long.html', './html/footer.html'])
+    .pipe(concat('post-quote-long.html'))
+    .pipe(gulp.dest('./dist/'));
+});
+
 
 
 // ... watch
 gulp.task('watch', function(){
-  gulp.watch(['./sass/**/*.sass', './html/*'], ['clean', 'sass', 'autoprefixer', 'build-posts-html', 'build-post-html', 'build-post-video-html', 'build-post-text-html', 'build-post-image-html', 'build-post-photoset-html', 'build-post-audio-html', 'build-post-chat-html', 'build-post-link-html']);
+  gulp.watch(['./sass/**/*.sass', './html/*'], ['clean', 'sass', 'autoprefixer', 'build-posts-html', 'build-post-html', 'build-post-video-html', 'build-post-text-html', 'build-post-image-html', 'build-post-photoset-html', 'build-post-audio-html', 'build-post-chat-html', 'build-post-link-html', 'build-post-quote-short-html', 'build-post-quote-long-html']);
 });
