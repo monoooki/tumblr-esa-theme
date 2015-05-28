ac009c17749b62fd496917a525bae4ef9a8be7c2
Kazuhiko Maeda <shirokumasensei@gmail.com>
2015-05-28 11:25:31 +0900

    テンプレ確認用のHTMLを整理

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var watch = require('gulp-watch');
 
-gulp.task('default', ['clean', 'sass', 'autoprefixer', 'build-posts-html', 'build-post-html', 'build-post-video-html', 'watch']);
+gulp.task('default', ['clean', 'sass', 'autoprefixer', 'build-posts-html', 'build-post-html', 'watch']);
 
 
 
@@ -47,73 +47,27 @@ gulp.task('build-posts-html', function() {
 });
 
 gulp.task('build-post-html', function() {
-  return gulp.src(['./html/header.html', './html/post.html', './html/footer.html'])
+  return gulp.src([
+      './html/header.html',
+      './html/post.html',
+      './html/posttypes/post-text.html',
+      './html/posttypes/post-image.html',
+      './html/posttypes/post-photoset.html',
+      './html/posttypes/post-link.html',
+      './html/posttypes/post-quote-short.html',
+      './html/posttypes/post-quote-long.html',
+      './html/posttypes/post-chat.html',
+      './html/posttypes/post-audio.html',
+      './html/posttypes/post-video.html',
+      './html/posttypes/post-answer.html',
+      './html/footer.html'
+    ])
     .pipe(concat('post.html'))
-    .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('build-post-video-html', function() {
-  return gulp.src(['./html/header.html', './html/post-video.html', './html/footer.html'])
-    .pipe(concat('post-video.html'))
-    .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('build-post-text-html', function() {
-  return gulp.src(['./html/header.html', './html/post-text.html', './html/footer.html'])
-    .pipe(concat('post-text.html'))
-    .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('build-post-image-html', function() {
-  return gulp.src(['./html/header.html', './html/post-image.html', './html/footer.html'])
-    .pipe(concat('post-image.html'))
-    .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('build-post-photoset-html', function() {
-  return gulp.src(['./html/header.html', './html/post-photoset.html', './html/footer.html'])
-    .pipe(concat('post-photoset.html'))
-    .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('build-post-audio-html', function() {
-  return gulp.src(['./html/header.html', './html/post-audio.html', './html/footer.html'])
-    .pipe(concat('post-audio.html'))
-    .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('build-post-chat-html', function() {
-  return gulp.src(['./html/header.html', './html/post-chat.html', './html/footer.html'])
-    .pipe(concat('post-chat.html'))
-    .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('build-post-link-html', function() {
-  return gulp.src(['./html/header.html', './html/post-link.html', './html/footer.html'])
-    .pipe(concat('post-link.html'))
-    .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('build-post-quote-short-html', function() {
-  return gulp.src(['./html/header.html', './html/post-quote-short.html', './html/footer.html'])
-    .pipe(concat('post-quote-short.html'))
-    .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('build-post-quote-long-html', function() {
-  return gulp.src(['./html/header.html', './html/post-quote-long.html', './html/footer.html'])
-    .pipe(concat('post-quote-long.html'))
-    .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('build-post-answer-html', function() {
-  return gulp.src(['./html/header.html', './html/post-answer.html', './html/footer.html'])
-    .pipe(concat('post-answer.html'))
     .pipe(gulp.dest('./dist/'));
 });
 
 
 // ... watch
 gulp.task('watch', function(){
-  gulp.watch(['./sass/**/*.sass', './html/*'], ['clean', 'sass', 'autoprefixer', 'build-posts-html', 'build-post-html', 'build-post-video-html', 'build-post-text-html', 'build-post-image-html', 'build-post-photoset-html', 'build-post-audio-html', 'build-post-chat-html', 'build-post-link-html', 'build-post-quote-short-html', 'build-post-quote-long-html', 'build-post-answer-html']);
+  gulp.watch(['./sass/**/*.sass', './html/*'], ['clean', 'sass', 'autoprefixer', 'build-posts-html', 'build-post-html']);
 });
